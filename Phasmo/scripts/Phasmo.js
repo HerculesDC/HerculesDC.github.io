ff8570de65b33745c3e94eb544fc4b5209f8a887
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2024-06-14 14:23:07 -0400

    add some js

--- Phasmo/scripts/Phasmo.js
+++ Phasmo/scripts/Phasmo.js
@@ -1,4 +1,14 @@
+const ghostLink = document.createElement("link");
+ghostLink.id = "ghostList";
+ghostLink.href = "../assets/json/ghostList.json";
+
+document.body.appendChild(ghostLink);
+
 function randomize(){
 	
+	let ghostListLink = document.getElementById("ghostList");
+	let ghostList = fetch(ghostListLink.href);
+	console.log(ghostList.length);
 }
 
+randomize();
