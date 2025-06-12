c3f505a4f2225a3c9abf30565af56ea6256fbe99
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2025-06-12 13:50:24 -0400

    forgot to change branches - fix some hardcode

--- DropARock/assets/scripts/DropARock.js
+++ DropARock/assets/scripts/DropARock.js
@@ -14,7 +14,6 @@ function setup(){
 	textAlign(CENTER, CENTER);
     frameRate(60);
 }
-
 //NOTE: This is relative to the calling HTML page location
 //var rock = loadImage("./assets/images/rock.png");
 //var nugget = loadImage("./assets/images/goldnugget.png")
@@ -30,22 +29,19 @@ class Rock{
 		this.chance = 0.1;
 		this.hasNugget = false;
 		this.currentState = 0; //0 - intro, 1 - idle, 2 - falling, 3 - broken;
-		this.stateFunctions = [this.intro, this.idle, this.fall, this.broken];
-		this.renderFunctions = [this.renderFull, this.renderBroken];
 		this.reset();
 	}
 	update(delta){
-		//this.stateFunctions[this.currentState](delta);
 		switch(this.currentState){
 			case 0:
-				this.y += 5;
-				if(this.y >= 255){
+				this.y += CANVAS_HEIGHT/2*delta;
+				if(this.y >= CANVAS_HEIGHT/5){
 					this.currentState = 1;
 				}
 				break;
 			case 2:
-				this.y += 12;
-				if(this.y >= CANVAS_HEIGHT-255){
+				this.y += CANVAS_HEIGHT*delta*1.25;
+				if(this.y > CANVAS_HEIGHT-255){
 					this.currentState = 3;
 				}
 				break;
@@ -53,10 +49,9 @@ class Rock{
 		}
 	}
 	render(){
-		//let currFunc = this.currentState == 3 ? 1 : 0;
 		if(this.currentState ==3){
 			if(this.hasNugget){
-				image(this.nugget, this.x, this.y);
+				image(this.nugget, this.x, this.y+100);
 			}
 			image(this.rock, this.x-200, this.y, 255, 512, 0, 0, 255, 512);
 			image(this.rock, this.x+200, this.y, 255, 512, 255, 0, 255, 512);
@@ -64,7 +59,9 @@ class Rock{
 		else{
 			image(this.rock, this.x, this.y);
 		}
-		//this.renderFunctions[currFunc](this.x, this.y, this.hasNugget);
+	}
+	requestStateChange(changeTo){
+		this.currentState = changeTo;
 	}
 	reset(){
 		let prob1 = Math.random();
@@ -75,40 +72,13 @@ class Rock{
 		console.log("The current rock "+hasNugget+" a nugget.");
 		this.requestStateChange(0);
 	}
-	requestStateChange(changeTo){
-		this.currentState = changeTo;
-	}
-	intro(delta){
-		this.y += 10;
-		if(this.y >= this.rh){ this.requestStateChange(1); } 
-	}
-	idle(delta){
-		//this.y = this.rock.height;
-	}
-	fall(delta){
-		this.y += delta;
-		if(this.y >= CANVAS_HEIGHT-this.rh){this.requestStateChange(3);}
-	}
-	broken(delta){
-		
-	}
-	renderFull(x, y){
-		image(this.rock, x, y);
-	}
-	renderBroken(x, y, nugget){
-		if(nugget){
-			image(this.nugget, x, y);
-		}
-		image(this.rock, x-200, y, 255, 512, 0, 0, 255, 512);
-		image(this.rock, x+200, y, 255, 512, 255, 0, 512, 512);
-	}
 };
 
 var r = new Rock(rock, nugget);
 
 function mouseClicked(){
 	switch(r.currentState){
-		case 1: r.currentState = 2; break;
+		case 1: r.requestStateChange(2); break;
 		case 3: r.reset(); break;
 		default: break;
 	}
@@ -116,21 +86,22 @@ function mouseClicked(){
 
 function displayText(rock){
 	fill(1.05, 0.5, 1.0);
-	textSize(CANVAS_HEIGHT/10);
+	let lowest = Math.min(CANVAS_WIDTH, CANVAS_HEIGHT);
+	textSize(lowest/10);
 	if(rock.currentState == 1){
 		text("TOUCH OR CLICK\nTO DROP ROCK", CANVAS_WIDTH/2, CANVAS_HEIGHT*(2/3));
 	}
 	if(rock.currentState == 3){
-		let msg = rock.hasNugget?"GOOD JOB!":"BETTER LUCK NEXT TIME...";
+		let msg = rock.hasNugget?"GOOD JOB!":"BETTER LUCK\nNEXT TIME...";
 		text(msg, CANVAS_WIDTH/2, CANVAS_HEIGHT/4);
-		textSize(CANVAS_HEIGHT/20);
+		textSize(lowest/20);
 		text("click or touch to reset", CANVAS_WIDTH/2, CANVAS_HEIGHT*0.975);
 	}
 }
 
 function draw(){
 	background(0, 0, 0.23);
-	r.update(1);
+	r.update(1/frameRate());
 	r.render();
 	displayText(r);
 }
