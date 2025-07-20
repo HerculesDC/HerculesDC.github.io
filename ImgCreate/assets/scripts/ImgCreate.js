f6a5b8a1656eaf3fcf189810d50986fc76fad819
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2025-07-19 20:43:46 -0400

    created a few iamges

--- ImgCreate/assets/scripts/ImgCreate.js
+++ ImgCreate/assets/scripts/ImgCreate.js
@@ -1,8 +1,10 @@
 new p5();
 
-var CANVAS_WIDTH = 512;//Math.floor(windowWidth*0.9825);
+const c_sz = 128;
+
+var CANVAS_WIDTH = c_sz>>1;//Math.floor(windowWidth*0.9825);
 var LPADDING = Math.floor((windowWidth-CANVAS_WIDTH)*0.3);
-var CANVAS_HEIGHT = 512;//Math.floor(windowHeight*.9125);
+var CANVAS_HEIGHT = c_sz;//Math.floor(windowHeight*.9125);
 var TPADDING = 75;
 
 function setup(){
@@ -28,7 +30,7 @@ function windowResized(){
 
 var fName = "";
 
-function mountain(){
+function mountains(){
 	noStroke();
 	fill(2.5, 0.7, 0.5);
 	beginShape();
@@ -40,8 +42,34 @@ function mountain(){
 	fName = "Mountain";
 }
 
+function mountain(){
+	let sz = CANVAS_WIDTH<<1;
+	for(let i = sz; i > 0; i-= 32){
+		noStroke();
+		fill(2.25, (i/sz), 1-(i/sz));
+		arc(CANVAS_WIDTH>>1, 0, i,i, 0.25*PI, 0.75*PI);
+	} 
+}
+
+function hill(){
+	let sz = CANVAS_WIDTH;
+	fill(2.25, 0.75, 0.75);
+	ellipse(CANVAS_WIDTH>>1, CANVAS_HEIGHT, sz, sz);
+}
+
+function tree(){
+	let cntr = CANVAS_WIDTH >> 1;
+	noStroke();
+	fill(0.5, 0.7, 0.8);
+	rect(cntr, CANVAS_HEIGHT*0.5, cntr*0.5, CANVAS_HEIGHT*0.25);
+	for(let th = CANVAS_WIDTH; th > 0; th -= 8){
+		fill(2, 0.5, 0.25+(0.75*th/CANVAS_WIDTH));
+		arc(cntr, 0, th*1.45, th*1.75, 0.35*PI, 0.65*PI);
+	}
+}
+
 function draw(){
-	mountain();
+	tree();
 }
 
 function mouseClicked(){save(fName+".png");}
